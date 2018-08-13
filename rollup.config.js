f7baa077dc371e972e67183ba5077f3c5aea545f
zhao wen qiang <13143410521@163.com>
2018-08-13 11:41:23 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from 'rollup-typescript';
+export default {
+    input: 'types/index.ts',
+    output: {
+        file: 'lib/index.js',
+        format: 'cjs'
+    },
+    plugins: [
+        typescript()
+    ]
+}
